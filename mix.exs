f1259c90b2aecbb11bcf00f6a5a67e93439852a0
Erik <e.ferrari@arkemis.it>
2024-04-23 16:53:50 +0200

    chore: set version to v0.3.0

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule ArkeAuth.MixProject do
   use Mix.Project
-  @version "0.1.16"
+  @version "0.3.0"
   @scm_url "https://github.com/arkemishub/arke-auth"
   @site_url "https://arkehub.com"
 
@@ -55,7 +55,7 @@ defmodule ArkeAuth.MixProject do
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:arke, "~> 0.1.13"}
+      {:arke, "~> 0.3.0"}
     ])
   end
 
