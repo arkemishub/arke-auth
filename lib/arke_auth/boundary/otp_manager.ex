567aa16f4f44d5d540aa3f279cfbcb4efe2f0eee
Erik <e.ferrari@arkemis.it>
2024-06-26 12:32:10 +0200

    chore: bypass otp

--- lib/arke_auth/boundary/otp_manager.ex
+++ lib/arke_auth/boundary/otp_manager.ex
@@ -15,8 +15,26 @@
 defmodule ArkeAuth.Boundary.OtpManager do
   @moduledoc false
   use Arke.Boundary.UnitManager
+  alias ArkeAuth.Core.Otp
+  alias Arke.QueryManager
 
   manager_id(:otp)
-  # set_registry_name(:parameter_registry)
-  # set_supervisor_name(:parameter_supervisor)
+
+  def get_code(project, member,action \\ "signin")
+  def get_code(project, member,action) when is_atom(action), do: get_code(project, member,to_string(action))
+  def get_code(project, member,action) do
+    case System.get_env("OTP_BYPASS_CODE") do
+      nil -> get_member_code(project,member,action)
+      "" ->  get_member_code(project,member,action)
+      value -> %{data: %{code: value, expiry_datetime: NaiveDateTime.utc_now() |> NaiveDateTime.add(300, :second) }}
+    end
+  end
+
+  defp get_member_code(project,member,action) do
+    QueryManager.get_by(project: project,arke: "otp",id: Otp.parse_otp_id(action, member.id),action: action)
+  end
+
+  def delete_otp(%Arke.Core.Unit{metadata: %{project: project}}=unit), do:   QueryManager.delete(project, unit)
+  def delete_otp(_unit), do:  nil
+
 end
