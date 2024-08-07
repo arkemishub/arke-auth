0f7f986a26d4a3be01246c28c7f3890b5e0bcbef
vittorio-reinaudo <vittorioreinaudo@Vittorios-MacBook-Pro.local>
2024-08-07 20:48:00 +0200

    feat: add new arke temporary token and management

---
+++ lib/arke_auth/core/temporary_token.ex
@@ -0,0 +1,49 @@
+# Copyright 2023 Arkemis S.r.l.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule ArkeAuth.Core.TemporaryToken do
+  @moduledoc """
+             Documentation for `TemporaryToken`.
+             """
+
+  alias Arke.QueryManager
+  alias Arke.Boundary.ArkeManager
+
+
+  use Arke.System
+
+  arke id: :otp do
+  end
+
+  def generate_token(project, id, action, expiry_datetime \\ nil) do
+    {:error, "not implemented"}
+  end
+
+  def generate_auth_token(project, member, duration \\ nil, is_reusable \\ false, opts \\ []) do
+    temp_arke = ArkeManager.get(:temporary_token, project)
+    expiration_datetime = calculate_expiration_datetime(duration)
+    data = [link_member: member.id, expiration_datetime: expiration_datetime, is_reusable: is_reusable] ++ opts
+    QueryManager.create(project, temp_arke, data)
+  end
+
+  defp calculate_expiration_datetime(duration) when is_nil(duration), do: add_from_now(System.get_env("DEFAULT_EXPIRATION_TEMPORARY_TOKEN", "1800"))
+  defp calculate_expiration_datetime(%{days: days}), do: add_from_now(days * 86400)
+  defp calculate_expiration_datetime(%{minutes: minutes}), do: add_from_now(minutes * 60)
+  defp calculate_expiration_datetime(%{days: days, minutes: minutes}), do: add_from_now(days * 86400 + minutes * 60)
+  defp calculate_expiration_datetime(duration), do: add_from_now(duration)
+
+  defp add_from_now(seconds) when is_binary(seconds), do: String.to_integer(seconds) |> add_from_now
+  defp add_from_now(seconds), do: NaiveDateTime.utc_now() |> NaiveDateTime.add(seconds, :second)
+end
+
