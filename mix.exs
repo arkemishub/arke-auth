ba029bb85a60ef23a5aba1f785a118eb6de1f5e9
Erik <e.ferrari@arkemis.it>
2025-02-14 14:15:43 +0100

    v0.3.5

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule ArkeAuth.MixProject do
   use Mix.Project
-  @version "0.3.4"
+  @version "0.3.5"
   @scm_url "https://github.com/arkemishub/arke-auth"
   @site_url "https://arkehub.com"
 
